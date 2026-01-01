a96316c88b733652cf44733e3f9b4a89814a3e3d
kidwa-ui <goodness.169@gmail.com>
2026-01-01 19:31:13 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,17 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    realtime: {
+      params: {
+        eventsPerSecond: 10
+      }
+    }
+  }
+)
 // ===== Auth Functions =====
 
 // สมัครสมาชิกด้วย Email + Password
